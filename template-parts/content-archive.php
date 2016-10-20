caf64ba1c21a42e8345def2305858b801419f0a8
alfredo navas <elpuas@gmail.com>
2016-10-20 19:49:10 -0400

    More Styles

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -10,13 +10,24 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+	<div class="entry-content">
+		<div class="col-md-3">
+		<?php if ( has_post_thumbnail()) : ?>
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+<?php the_post_thumbnail('medium'); ?>
+</a>
+<?php endif; ?>
+</div>
+<div class="col-md-9">
 	<header class="entry-header">
 
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 		else :
-			the_title( '<h2 class="entry-title">', '</h2>' );
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
@@ -27,16 +38,6 @@
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<div class="col-md-4">
-		<?php if ( has_post_thumbnail()) : ?>
-<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-<?php the_post_thumbnail('medium'); ?>
-</a>
-<?php endif; ?>
-</div>
-<div class="col-md-8">
 		<?php
 			the_excerpt( sprintf(
 				/* translators: %s: Name of current post. */
