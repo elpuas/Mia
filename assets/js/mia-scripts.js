d868f63a69ec7d0043acb57a804b923d2ee539fd
Alfredo Navas <elpuas@gmail.com>
2016-10-24 09:55:22 -0600

    Styles Update

--- assets/js/mia-scripts.js
+++ assets/js/mia-scripts.js
@@ -33,3 +33,13 @@ jQuery(function ($) {
 
 // Slogan
 console.log('Made with passion and lots of coffee, from the beautiful Costa Rica');
+
+// Analytics
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-86116984-1', 'auto');
+  ga('send', 'pageview');
